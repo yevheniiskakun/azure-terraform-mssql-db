32b23783e7fe15065b03b0fc07dc552aba00c09e
yevheniiskakun <yevhenii.skakun@gmail.com>
2025-05-28 23:54:55 +0200

    Added name randomiser, comments and fixed naming issues

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ resource "random_password" "password" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
+resource "random_pet" "name" {
+  
+}
+
 resource "azurerm_resource_group" "mssql-rg" {
   name     = "mssql-db-resources"
   location = var.location
@@ -13,26 +17,26 @@ resource "azurerm_resource_group" "mssql-rg" {
 
 
 resource "azurerm_key_vault" "mssql-kv" {
-  name                       = "mssql-key-vault"
+  name                       = "kv-${random_pet.name.id}"
   location                   = azurerm_resource_group.mssql-rg.location
   resource_group_name        = azurerm_resource_group.mssql-rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
 
-  enable_rbac_authorization = true # https://learn.microsoft.com/en-us/azure/key-vault/general/rbac-guide?tabs=azure-cli
+  enable_rbac_authorization = true # Do not forgot to add yourself one of the roles: https://learn.microsoft.com/en-us/azure/key-vault/general/rbac-guide?tabs=azure-cli
 
 }
 
 resource "azurerm_key_vault_secret" "mssql-server-login" {
   name         = "mssql-server-login"
   value        = var.server_admin_login
-  key_vault_id = azurerm_resource_group.mssql-rg.id
+  key_vault_id = azurerm_key_vault.mssql-kv.id
 }
 
 resource "azurerm_key_vault_secret" "mssql-server-password" {
   name         = "mssql-server-password"
   value        = random_password.password.result
-  key_vault_id = azurerm_resource_group.mssql-rg.id
+  key_vault_id = azurerm_key_vault.mssql-kv.id
 }
 
