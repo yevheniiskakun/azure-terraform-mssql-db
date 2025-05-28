1773aa4d18b7f7f94bb03d02fe20d0526e714528
yevheniiskakun <yevhenii.skakun@gmail.com>
2025-05-29 00:20:05 +0200

    Added MSSQL server and DB

--- main.tf
+++ main.tf
@@ -40,3 +40,27 @@ resource "azurerm_key_vault_secret" "mssql-server-password" {
   key_vault_id = azurerm_key_vault.mssql-kv.id
 }
 
+resource "azurerm_mssql_server" "mssql" {
+  name                         = "sqlserver-${random_pet.name.id}"
+  resource_group_name          = azurerm_resource_group.mssql-rg.name
+  location                     = azurerm_resource_group.mssql-rg.location
+  version                      = "12.0"
+
+  administrator_login          = azurerm_key_vault_secret.mssql-server-login.value
+  administrator_login_password = azurerm_key_vault_secret.mssql-server-password.value
+}
+
+resource "azurerm_mssql_database" "mssql-db" {
+  name         = "db-${random_pet.name.id}"
+  server_id    = azurerm_mssql_server.mssql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
